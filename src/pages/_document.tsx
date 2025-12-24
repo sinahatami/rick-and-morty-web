751123df60b5ccaf70357542e3ba063d64a071fa
sina hatami <sina13781999@gmail.com>
2025-12-24 17:14:14 +0100

    First Commit!

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,14 @@
-import { Head, Html, Main, NextScript } from 'next/document'
+import { Head, Html, Main, NextScript } from "next/document";
 
 const Document = () => (
 	<Html lang="en">
 		<Head />
 		<body>
 			<Main />
+			<h1>Hello World!</h1>
 			<NextScript />
 		</body>
 	</Html>
-)
+);
 
-export default Document
+export default Document;
